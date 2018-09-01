f368bfa7d267b8782e367a9a6b8a67ede6e16f55
Cassandra <uncertainkitten@gmail.com>
2018-08-31 23:12:49 -0400

    Finish Linked List class

--- my_little_hashmap/lib/p04_linked_list.rb
+++ my_little_hashmap/lib/p04_linked_list.rb
@@ -13,49 +13,99 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @prev.next = @next unless @next.nil? || @prev.nil?
+    @next.prev = @prev unless @prev.nil? || @next.nil?
+    @key = nil
+    @val = nil
+    @next = nil
+    @prev = nil
   end
 end
 
 class LinkedList
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
+
+  include Enumerable
 
   def [](i)
     each_with_index { |link, j| return link if i == j }
     nil
   end
 
+  def []=(i, val)
+    each_with_index { |link, j| link.val = val if i == j}
+    nil
+  end
+
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    return true if get(key).nil? == false
+    false
   end
 
   def append(key, val)
+    child = Node.new(key, val)
+    if empty?
+      @head.next = child
+      child.prev = @head
+      child.next = @tail
+      @tail.prev = child
+    else
+      child.prev = self.last
+      self.last.next = child
+      @tail.prev = child
+      child.next = @tail
+    end
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
+    nil
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+        break
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    item = self.first
+    until item == @tail
+      prc.call(item)
+      item = item.next
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
