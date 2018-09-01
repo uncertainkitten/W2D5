5c9a397957e22e555b5aa6b7332c58c13b412006
Cassandra <uncertainkitten@gmail.com>
2018-08-31 23:42:08 -0400

    Finish HashMap class

--- my_little_hashmap/lib/p05_hash_map.rb
+++ my_little_hashmap/lib/p05_hash_map.rb
@@ -3,16 +3,28 @@ require_relative 'p04_linked_list'
 class HashMap
   attr_reader :count
 
+  include Enumerable
+
   def initialize(num_buckets = 8)
     @store = Array.new(num_buckets) { LinkedList.new }
     @count = 0
   end
 
   def include?(key)
+    bucket(key).include?(key)
   end
 
   def set(key, val)
-    bucket(key).append(key, val)
+    if include?(key)
+      bucket(key).update(key, val)
+    else
+      bucket(key).append(key, val)
+      @count += 1
+    end
+
+    if count >= num_buckets
+      resize!
+    end
   end
 
   def get(key)
@@ -20,18 +32,24 @@ class HashMap
   end
 
   def delete(key)
+    bucket(key).remove(key)
+    @count -= 1
   end
 
-  def each
+  def each(&prc)
+    @store.each do |bucket|
+      bucket.each do |item|
+        prc.call(item.key, item.val)
+      end
+    end
   end
 
-  # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -43,6 +61,10 @@ class HashMap
   end
 
   def resize!
+    @store += Array.new(num_buckets) { LinkedList.new }
+    self.each do |key, value|
+      set(key, value)
+    end
   end
 
   def bucket(key)
