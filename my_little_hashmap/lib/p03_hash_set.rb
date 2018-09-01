14dcd3aa779187f6c061615110d1f6b5f6e18752
Cassandra <uncertainkitten@gmail.com>
2018-08-31 20:00:46 -0400

    Finish HashSet class based on ResizingIntSet class

--- my_little_hashmap/lib/p03_hash_set.rb
+++ my_little_hashmap/lib/p03_hash_set.rb
@@ -7,18 +7,36 @@ class HashSet
   end
 
   def insert(key)
+    h_key = key.hash
+    item = key
+    unless include?(h_key)
+      @count += 1
+      self[h_key] << item
+      if @count >= num_buckets
+        resize!
+      end
+    end
   end
 
   def include?(key)
+    h_key = key.hash
+    item = key
+    self[h_key].include?(item)
   end
 
   def remove(key)
+    h_key = key.hash
+    item = key
+    if include?(item)
+      @count -= 1
+      self[h_key].delete(item)
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +44,11 @@ class HashSet
   end
 
   def resize!
+    temp = @store.flatten
+    @store = Array.new(num_buckets*2) {Array.new}
+    temp.each do |el|
+      @count -= 1
+      insert(el)
+    end
   end
 end
